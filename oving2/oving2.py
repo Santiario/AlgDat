9a750e47ba846cc1ee9bcd3a7f7e276d1db5d2f2
Vemund Santi <vemund@santi.no>
2015-09-08 11:31:37 +0200

    Shortened all variablenames in bfs and dfs

--- oving2/oving2.py
+++ oving2/oving2.py
@@ -4,37 +4,37 @@ from sys import stdin
 
 class Node:
     def __init__(self):
-        self.barn = []
-        self.ratatosk = False
-        self.distance = -1
+        self.b = []
+        self.a = False
+        self.d = -1
 
 
-def dfs(rot):
-    node_stakk = deque()
-    rot.distance = 0
-    node_stakk.append(rot)
-    while node_stakk:
-        node = node_stakk.pop()
-        if node.ratatosk:
-            return node.distance
-        for barn in node.barn:
-            if barn.distance == -1:
-                barn.distance = node.distance + 1
-                node_stakk.append(barn)
+def dfs(r):
+    ns = deque()
+    r.d = 0
+    ns.append(r)
+    while ns:
+        n = ns.pop()
+        if n.a:
+            return n.d
+        for b in n.b:
+            if b.d == -1:
+                b.d = n.d + 1
+                ns.append(b)
 
 
-def bfs(rot):
-    node_ko = deque()
-    rot.distance = 0
-    node_ko.appendleft(rot)
-    while node_ko:
-        node = node_ko.pop()
-        if node.ratatosk:
-            return node.distance
-        for child in node.barn:
-            if child.distance == -1:
-                child.distance = node.distance + 1
-                node_ko.appendleft(child)
+def bfs(r):
+    nk = deque()
+    r.d = 0
+    nk.appendleft(r)
+    while nk:
+        n = nk.pop()
+        if n.a:
+            return n.d
+        for b in n.b:
+            if b.d == -1:
+                b.d = n.d + 1
+                nk.appendleft(b)
 
 
 def main():
@@ -45,12 +45,12 @@ def main():
         noder.append(Node())
     start_node = noder[int(stdin.readline())]
     ratatosk_node = noder[int(stdin.readline())]
-    ratatosk_node.ratatosk = True
+    ratatosk_node.a = True
     for linje in stdin:
         tall = linje.split()
         temp_node = noder[int(tall.pop(0))]
         for barn_nr in tall:
-            temp_node.barn.append(noder[int(barn_nr)])
+            temp_node.b.append(noder[int(barn_nr)])
 
     if funksjon == 'dfs':
         print dfs(start_node)
