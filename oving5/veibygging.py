40ff6cb31b317e2785b2492c5ef5ef653b62880d
Vemund Santi <vemund@santi.no>
2015-10-02 17:40:24 +0200

    Implementer O(n^3) Prims algorithm

--- oving5/veibygging.py
+++ oving5/veibygging.py
@@ -1,29 +1,47 @@
-__author__ = 'Vemund'
-
 from sys import stdin
+__author__ = 'vemund'
 
 Inf = float(1e3000)
-False = 0
-True = 1
+
 
 def mst(nm):
-    pass
+    nodeliste = set()
+    nodeliste.add(0)
+    heaviest = -Inf
+    while True:
+        shortest = Inf
+        neste_kant = Inf
+        for vertice in nodeliste:
+            for edge, weight in enumerate(nm[vertice]):
+                if weight < shortest:
+                    if vertice not in nodeliste and edge in nodeliste:
+                            neste_kant = vertice
+                            shortest = weight
+                    elif vertice in nodeliste and edge not in nodeliste:
+                            neste_kant = edge
+                            shortest = weight
+        if shortest == Inf:
+            break
+        else:
+            nodeliste.add(neste_kant)
+            if shortest > heaviest:
+                heaviest = shortest
+    return heaviest
 
 
-
-linjer = []
-for str in stdin:
-    linjer.append(str)
-n = len(linjer)
-nabomatrise = [None] * n
-node = 0
-for linje in linjer:
-    nabomatrise[node] = [Inf] * n
-    for k in linje.split():
-        data = k.split(':')
-        nabo = int(data[0])
-        vekt = int(data[1])
-        nabomatrise[node][nabo] = vekt
-    node += 1
-    print nabomatrise
-print mst(nabomatrise)
+def main():
+    linjer = []
+    for string in stdin:
+        linjer.append(string)
+    n = len(linjer)
+    nabomatrise = [None] * n
+    node = 0
+    for linje in linjer:
+        nabomatrise[node] = [Inf] * n
+        for k in linje.split():
+            data = k.split(':')
+            nabo = int(data[0])
+            vekt = int(data[1])
+            nabomatrise[node][nabo] = vekt
+        node += 1
+    print mst(nabomatrise)
