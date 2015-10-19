5c1171625ef8f916b7753c822f8012816342f83b
Vemund Santi <vemund@santi.no>
2015-10-19 14:23:40 +0200

    Merged all functions into one, giant function: main

--- oving8/mumien.py
+++ oving8/mumien.py
@@ -1,56 +1,9 @@
 __author__ = 'Vemund'
 
+
 def main():
+
     from sys import stdin
-
-    def remove_highest(noder, weights):
-        highest = weights[noder[0]]
-        index = noder[0]
-        for i in noder:
-            if weights[i] > highest:
-                highest = weights[i]
-                index = i
-        noder.pop(noder.index(index))
-        return index
-
-    def relax(edge_from, edge_to, weights, parents, sannsynligheter):
-        if weights[edge_to] < weights[edge_from]*sannsynligheter[edge_to]:
-            weights[edge_to] = weights[edge_from]*sannsynligheter[edge_to]
-            parents[edge_to] = edge_from
-            #print 'Setter node', edge_to, 'til aa ha parent', edge_from
-
-    def beste_sti(nabomatrise, sannsynligheter):
-        parents = [None]*len(sannsynligheter)
-        weights = [0]*len(sannsynligheter)
-        weights[0] = sannsynligheter[0]
-        noder = [x for x in range(len(sannsynligheter))]
-        visited = set()
-        while len(noder) > 0:
-            node = remove_highest(noder, weights)
-            if node in visited:
-                continue
-            visited.add(node)
-            #print 'Tar for oss node nr', node, 'som har (beste) verdi', weights[node]
-            #print sorted(visited)
-            for i in range(len(nabomatrise)):
-                if nabomatrise[node][i] == 1:
-                    relax(node, i, weights, parents, sannsynligheter)
-            if node == len(nabomatrise) - 1:
-                break
-        path = [str(len(parents) - 1)]
-        parent = parents[-1]
-        changed = False
-        while parent is not None:
-            path.append(str(parent))
-            parent = parents[parent]
-            changed = True
-        if changed:
-            return "-".join(path[::-1])
-        else:
-            return '0'
-
-
-
 
     n = int(stdin.readline())
     sannsynligheter = map(float, stdin.readline().split())
@@ -61,8 +14,39 @@ def main():
         for nabo in naboer:
             naborad[nabo] = 1
         nabomatrise.append(naborad)
-    #for rad in nabomatrise:
-        #print rad
-    print beste_sti(nabomatrise, sannsynligheter)
+
+    parents = [None]*len(sannsynligheter)
+    weights = [0]*len(sannsynligheter)
+    weights[0] = sannsynligheter[0]
+    noder = [x for x in range(len(sannsynligheter))]
+    visited = set()
+    while len(noder) > 0:
+        highest = weights[noder[0]]
+        node = noder[0]
+        for i in noder:
+            if weights[i] > highest:
+                highest = weights[i]
+                node = i
+        noder.pop(noder.index(node))
+        if node in visited:
+            continue
+        visited.add(node)
+        for i in range(len(nabomatrise)):
+            if nabomatrise[node][i] == 1 and weights[i] < weights[node]*sannsynligheter[i]:
+                weights[i] = weights[node]*sannsynligheter[i]
+                parents[i] = node
+        if node == len(nabomatrise) - 1:
+            break
+    path = [str(len(parents) - 1)]
+    parent = parents[-1]
+    changed = False
+    while parent is not None:
+        path.append(str(parent))
+        parent = parents[parent]
+        changed = True
+    if changed:
+        print "-".join(path[::-1])
+    else:
+        print '0'
 
 main()
