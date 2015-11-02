c155c527ac9bf0563ea9b024a85da783b18c7d49
Vemund Santi <vemund@santi.no>
2015-11-02 17:34:31 +0100

    Added oving10

---
+++ oving10/alletilalle.py
@@ -0,0 +1,16 @@
+from sys import stdin, maxint
+
+def korteste_rute(rekkefolge, nabomatrise, byer):
+    pass
+
+    # SKRIV DIN KODE HER
+
+testcases = int(stdin.readline())
+for test in range(testcases):
+    byer = int(stdin.readline())
+    rekkefolge = [int(by) for by in stdin.readline().split()]
+    nabomatrise = []
+    for by in range(byer):
+        pass
+    # SKRIV DIN KODE HER
+    print korteste_rute(rekkefolge, nabomatrise, byer)
